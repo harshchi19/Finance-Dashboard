7017b4340e17b8f8d69ace9d1fdfdc26bc3c5b0c
Harsh chitaliya <124803206+harshchi19@users.noreply.github.com>
2025-02-26 10:17:37 +0530

    Update infomatrix.py

--- infomatrix.py
+++ infomatrix.py
@@ -40,7 +40,7 @@ def infomatrix():
 
 
     st.sidebar.title("FinRupee")
-    page = st.sidebar.radio("Checkout", ["Home", "AI Assistant", "Portfolio", "AI Finance Management","Market Simulation","Expense Retrieval","Dupont Analysis","Algorithm Strategies","Business Intelligence Tool","Business Analysts","Insurance","CryptoCurrency","Unified Banking using Blockchain"])
+    page = st.sidebar.radio("Checkout", ["Home", "AI Assistant", "Portfolio","Market Simulation","Expense Retrieval","Algorithm Strategies","Business Intelligence Tool"])
 
 
     def load_css(file_name):
